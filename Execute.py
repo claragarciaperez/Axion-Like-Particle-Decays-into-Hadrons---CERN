f492cabd0a85844b921a280b5468f97adc3cd2c5
claragarciaperez <claragarperez@gmail.com>
2025-08-12 16:53:06 +0200

    Update Execute.py

--- Execute.py
+++ Execute.py
@@ -20,14 +20,19 @@ T_data = data_gs[:,0]
 gsinterp= interp1d(T_data, gstar, kind='cubic', fill_value="extrapolate")
 
 h_bar = 6.582e-22 #MeV
+Y_SM = 0.2533435042576797 
+Neff_SM = 3.0287538204143387
+zeta3   = 1.202056903159594  # ζ(3)
+
 
 #%%
-prop = np.logspace(np.log10(1e-2),np.log10(1), 1) #Values of rhoa/rhoSM
-taua = np.logspace(np.log10(0.03),np.log10(10), 1) #Values of taua
 T_0 = 20 #Initial temperature in MeV
 T_f = 0.01 #Final temperature in MeV
 ma = 400 #ALP mass
-Br = 0.3 #Branching ratio
+Br = defin.Br_pion(ma) #Branching ratio
+upper_bound_prop = 2 * zeta3 / np.pi**2 * T_0**3*(gsinterp(T_0)/80)/(defin.rho_SM(T_0,gsinterp(T_0)))*ma
+prop = np.logspace(np.log10(1e-6),np.log10(1), 10) #Values of rhoa/rhoSM
+taua = np.logspace(np.log10(0.01),np.log10(20), 10) #Values of taua
 
 Temperature_values = np.logspace(np.log10(T_0), np.log10(T_f), num=1000)
 
@@ -39,15 +44,26 @@ for t in t_list:
     time_seconds.append(t*h_bar)
 
 Neff = []
-Y = []
+Y_api = []
+Y_a = []
 for p in prop:
     auxN = []
-    auxY = []
+    auxYapi = []
+    auxYa = []
     for t in taua:
-        He, neff = Xn.He_neff(T_0,t_list,time_seconds,p,t,ma,Br)
+        Heapi, Hea, neff = Xn.He_neff(T_0,T_f,t_list,time_seconds,p,t,ma,Br)
         auxN.append(neff)
-        auxY.append(He)
-        print(p,t,He, neff)
+        auxYapi.append((Heapi- Y_SM)/Y_SM)
+        auxYa.append((Hea- Y_SM)/Y_SM)
+        print(p,t,(Heapi- Y_SM)/Y_SM, (Hea- Y_SM)/Y_SM, neff)
     Neff.append(auxN)
-    Y.append(auxY)
+    Y_api.append(auxYapi)
+    Y_a.append(auxYa)
+
+
+
+# %%
+defin.paint_Neff(Neff,taua,prop,ma,Br)
+defin.paint_Xn_pions(Y_api,taua,prop,ma,Br)
+defin.paint_Xn_no_pions(Y_a,taua,prop,ma,Br)
 # %%
