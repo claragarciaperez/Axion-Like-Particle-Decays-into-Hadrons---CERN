11f903b2075127e26adbeae393711441b1702433
claragarciaperez <claragarperez@gmail.com>
2025-08-25 21:09:42 +0200

    Update module_na0.py

--- module_na0.py
+++ module_na0.py
@@ -37,35 +37,127 @@ MeV_to_m = 5.067730717e12    # 1 m = 5.0677e12 MeV^-1
 
 #%%
 class initial_Abundance_ALP:
+    """
+        Class to calculate the initial abundance of alps
+
+        Parameters
+        ----------
+            T_ini: float
+                Initial temperature in which the ALPs start to appear. (MeV)
+            T_0: float
+                Initial decaying temperature (end of integration). (MeV)
+            m: float
+                Mass of the ALP (MeV)
+            gstar_interp:
+                Interpolator g*(T) (log-log interpolator with T in MeV)
+            entropy_interp: 
+                Interpolator s(T)/(T^3)
+            prop_charged_interp: 
+                Inerpolator g*_q(T)(log-log interpolator with T in MeV)
+            Temperature_interp: 
+                Interpolator T(t) (log-log interpolator with T in MeV and t in MeV^-1)
+        """
     def __init__(self,T_ini, T_0,m,gstar_interp,entropy_interp,prop_charged_interp, Temperature_interp ):
+
         Temp_aux = np.logspace(np.log10(T_ini),np.log10(T_0),1000)
         n_axionseq = [defin.n_axion_eq(T,m) for T in Temp_aux]
-        self.n_axionseq_interp = interp1d(Temp_aux,n_axionseq,kind='linear',fill_value="extrapolate")
+        self.n_axionseq_interp = interp1d(Temp_aux,n_axionseq,kind='cubic',fill_value="extrapolate")
         self.entropy_interp = entropy_interp
         self.prop_charged_interp = prop_charged_interp
         self.Temperature_interp  = Temperature_interp 
         self.gstar_interp = gstar_interp
 
     def g_value(self,m,tau_a):
+        """
+        Calculates the axion-photon coupling. g = np.sqrt(64*np.pi/m**3/tau_a)
+
+        Parameters
+        ----------
+            tau_a: float
+                Lifetime of the ALP in MeV^-1
+            m: float
+                Mass of the ALP (MeV)
+        """
         return np.sqrt(64*np.pi/m**3/tau_a)
 
 
     def Gamma_q(self,g,T,gq,m):
-        m_gamma = (e*T*np.sqrt(gq))/3
-        return ((alpha*g**2*gq*T**3)/36)*(np.log(T**2/m_gamma**2) + 0.82)*np.exp(-m/T)
+        """
+        Calculates the axion production rate via scattering of a charged particle q + gamma <--> q + a
+        Parameters
+        ----------
+            g: float
+                Axion photon coupling in MeV^-2
+            T: float
+                Temperature (MeV)
+            gq: float
+                Effective number of relativistic charged particles
+            m: float
+                mass of the ALP (MeV)
+        """
+        m_gamma = (e*T*np.sqrt(gq))/6 #Cuidado si 3 o 6
+        #return ((alpha*g**2*gq*T**3)/36)*(np.log(T**2/m_gamma**2) + 0.82)*np.exp(-m/T)
+        return ((alpha*g**2*gq*T**3)/36)*(np.log(T**2/m_gamma**2) + 0.82)
 
     def dYa_dt(self,Ya,t,m,tau_a):
+        """
+        Differential equation for calcualting Ya(T). dYa/dt = Gamma_q(Y_aeq - Ya)
+
+        Parameters
+        ----------
+            Ya: float
+                na/s, being na number density of ALPS and s entropy density.
+            t: float
+                Time (MeV^-1)
+            m: float
+                Mass of the ALP (MeV)
+            tau_a: float
+                Lifetime of the ALP (MeV)
+        """
         T = np.exp(self.Temperature_interp(np.log(t)))
         g = self.g_value(m,tau_a)
-        gq = np.exp(self.gstar_interp(np.log(T)))*np.exp(self.prop_charged_interp(np.log(T)))
+        #gq = np.exp(self.gstar_interp(np.log(T)))*np.exp(self.prop_charged_interp(np.log(T)))
+        gq = np.exp(self.prop_charged_interp(np.log(T)))
         s =(self.entropy_interp(T))*T**3
         G_q  = self.Gamma_q(g,T,gq,m)
-        nq =(zeta3/np.pi**2)*gq*T**3
-        Yq = nq/s
-        return G_q*Yq*(1-Ya/(self.n_axionseq_interp(T)/s))
+        #nq =(zeta3/np.pi**2)*gq*T**3
+        #Yq = nq/s
+        #return G_q*Yq*(1-Ya/(self.n_axionseq_interp(T)/s))
+        return G_q*((self.n_axionseq_interp(T)/s) - Ya)
 
 # %%
 def solve_dYa_dt (m, taua,Tini, T_0, times,entropy_interp,prop_charged_interp,gstar_interp, Temperature_interp, Ya_0 = 0):
+    """
+        Calculates abundance of ALPs at a temperature T0.
+
+        Parameters
+        ----------
+            m: float
+                Mass of the ALP (MeV)
+            taua: float
+                Lifetime of the ALP (MeV)
+            Tini: float
+                Initial integrarion temperature or Reheating temperature (MeV)
+            times: float
+                List with times to integrate in MeV^-1
+            entropy_interp: 
+                Interpolator s(T)/(T^3)
+            prop_charged_interp: 
+                Inerpolator g*_q(T)(log-log interpolator with T in MeV)
+            gstar_interp:
+                Interpolator g*(T) (log-log interpolator with T in MeV)
+            Temperature_interp: 
+                Interpolator T(t) (log-log interpolator with T in MeV and t in MeV^-1)
+            Y_a0: float
+                Ya(Tini). Default = 0
+
+        Return
+        ----------
+            rho_a(T_0)/rho_SM: float
+                Proportion of energy density at T_0 of ALPs
+            
+    """
+    
     ini_ALP = initial_Abundance_ALP(Tini, T_0,m,gstar_interp,entropy_interp,prop_charged_interp, Temperature_interp)
 
     sol =odeint(ini_ALP.dYa_dt, y0 =[Ya_0], t = times, args = (m,taua/h_bar),rtol = 1e-8, atol = 1e-10)
@@ -73,34 +165,3 @@ def solve_dYa_dt (m, taua,Tini, T_0, times,entropy_interp,prop_charged_interp,gs
     return (sol[:,0][-1]*entropy_interp(T_0)*T_0**3*m/(defin.rho_SM(T_0,np.exp(gstar_interp(np.log(T_0))))))
 
 
-#%%
-if __name__ == "__main__":
-    Tini = 1e15
-    T_0 = 20
-    Ya_0 = 0
-    m = 290
-    taua = 0.01
-    
-    gstar_interp, entropy_interp,prop_charged_interp,Temperature_interp = defin.interpolate_na0(Tini,T_0)
-
-    Y_a0 = defin.n_axion_eq(Tini,m)/(entropy_interp(Tini)*Tini**3)
-
-
-    times_fin = np.logspace(np.log10(defin.time_RD(Tini, np.exp(gstar_interp(np.log(Tini))))), np.log10(defin.time_RD(T_0, np.exp(gstar_interp(np.log(T_0))))),10000)
-
-    ini_ALP = initial_Abundance_ALP(Tini, T_0,m,gstar_interp,entropy_interp,prop_charged_interp, Temperature_interp)
-
-    sol =odeint(ini_ALP.dYa_dt, y0 =[Y_a0], t = times_fin, args = (m,taua/h_bar))
-
-    print(sol[:,0][-1]*entropy_interp(T_0)*T_0**3*m/(defin.rho_SM(T_0,np.exp(gstar_interp(np.log(T_0))))))
-
-    T = []
-    for t in times_fin:
-        T.append(np.exp(Temperature_interp(np.log(t))))
-    plt.plot(T,sol)
-    plt.xscale('log')
-    plt.ylabel(r'$Y_a$')
-    plt.xlabel(r'T($MeV$)')
-    plt.gca().invert_xaxis()
-
-# %%
